dc93351a87c32749ea80eaa164257de08e383b02
Emre Ünsal <emre@Emres-MacBook-Pro.local>
2023-05-26 22:34:07 +0300

    clear Faulty function and fixed sky box bug

--- src/App.jsx
+++ src/App.jsx
@@ -11,42 +11,38 @@ function App() {
   const [words, setWords] = useState();
   const [letters, setLetters] = useState("");
   const [showWord, setShowWord] = useState([]);
-  const [count, setCount] = useState(3);
-  const [boxPosition, setBoxPosition] = useState([]);
+  const [timer, setTimer] = useState(3);
 
   const intervalId = useRef(null);
   const mockLevel = useRef(null);
-  const skyInterval = useRef([]);
 
   useEffect(() => {
-    if(showWord.includes(letters)){
-      const result = showWord.filter((item)=> item !== letters);
-      if(result.length || showWord[0] === letters){
-       setShowWord(result);
+    if (showWord.includes(letters)) {
+      const result = showWord.filter((item) => item !== letters);
+      if (result.length || showWord[0] === letters) {
+        setShowWord(result);
         setLetters("");
       }
     }
-
   }, [letters]);
 
   useEffect(() => {
-    if (count === 0) {
+    if (timer === 0) {
       clearInterval(intervalId.current);
       intervalId.current = null;
       startGame(level);
     }
-  }, [count]);
+  }, [timer]);
 
   useEffect(() => {
     if (mockLevel.current < 0) {
       clearInterval(intervalId.current);
       intervalId.current = null;
       mockLevel.current = null;
-      setBoxPosition([]);
       return;
     }
   }, [mockLevel.current]);
-  
+
   useEffect(() => {
     if (
       showWord.length === 0 &&
@@ -55,20 +51,25 @@ function App() {
     ) {
       setLevel((prev) => prev + 1);
       startGame(level + 1);
+    } else if(showWord.length !== 0) {
+      console.log("tick")
+      const removeBoxInterval = setInterval(() => {
+        const updatedBox = showWord.map((box) => {
+          const newTop = box.top + 5;
+          if (newTop >= 200) {
+            return { ...box, visible: false };
+          }
+          return { ...box, top: newTop };
+        });
+        setShowWord(updatedBox);
+      }, 100);
+      return ()=> clearInterval(removeBoxInterval);
     }
   }, [showWord]);
-  useEffect(() => {
-    if(boxPosition.length){
-      startBoxSky(boxPosition)
-      return;
-    }
-    clearInterval(skyInterval);
-  }, [boxPosition])
-  
 
   const startClickHandler = () => {
     intervalId.current = setInterval(() => {
-      setCount((prev) => prev - 1);
+      setTimer((prev) => prev - 1);
     }, 1000);
   };
 
@@ -76,77 +77,59 @@ function App() {
     const result = generateRandomWord(3, level * 3);
     setWords(result);
     mockLevel.current = level * 3 - 1;
-    windowShowWord(result);
+    generateBox(result);
   };
 
-  const windowShowWord = (result) => {
+  const generateBox = (result) => {
     let count = 0;
     intervalId.current = setInterval(() => {
       const prevMockLevel = Number(mockLevel.current);
-      generateBoxPosition(100);
+      const xPostion = getRandomNumberInRange(0, 100);
       setShowWord((prev) => {
         mockLevel.current = prevMockLevel - 1;
-        return [...prev, result[prevMockLevel]];
+        return [
+          ...prev,
+          {
+            word: result[prevMockLevel],
+            top: 10,
+            left: xPostion,
+            visible: true,
+          },
+        ];
       });
       count = count + 1;
     }, 3000);
   };
 
-  const generateBoxPosition = (limit,showWords)=>{
-    console.log("abc show words", showWords);
-    const randomNumber = Math.floor(Math.random() * limit);
-    if(boxPosition.some((item)=> item.left === randomNumber)){
-        generateBoxPosition();
-        return;
-    }
-    setBoxPosition((prev) => [...prev, { left: randomNumber, top:0 }]);
-     return;
-  } 
-
-  const startBoxSky = (positions) => {
-    console.log("function çalıştı", positions);
-    let newPositions = positions;
-    skyInterval.current = setInterval(() => {
-       newPositions.map((position,index)=> {
-        console.log("positions map", position);
-        if(position.top >= 50){
-          setShowWord(showWord.filter((word,_index)=> index !== _index))
-          newPositions = newPositions.filter((position,__index)=> index !== __index);
-          console.log("if içine girdi", position.top);
-          setBoxPosition(newPositions);
-        }
-        
-        position.top = position.top + 5
-        console.log("if dışı pos", newPositions);
-        setBoxPosition(newPositions);
-       });
-       
-    }, 5000);
-  }
+  const getRandomNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
   return (
     <div className="App">
       <div className="container">
         <Menu startClickHandler={startClickHandler} />
-        {!menuVisible && count !== 0 && <div className="cont">{count}</div>}
+        {!menuVisible && timer !== 0 && <div className="cont">{timer}</div>}
         <div className="fly-body">
-          {showWord.map((word, index) => {
-            return (
-              <div
-                className="word-box"
-                style={{ left: `${boxPosition[index]?.left}%`, top: `${boxPosition[index]?.top}px`}}
-                key={index}
-              >
-                {word.split("").map((letter, index) => (
-                  <span
-                    key={index}
-                    className={letters[index] === letter ? "active" : ""}
-                  >
-                    {letter}
-                  </span>
-                ))}
-              </div>
-            );
+          {showWord.map((box, index) => {
+            if (box.visible) {
+              return (
+                <div
+                  className="word-box"
+                  style={{ left: `${box.left}%`, top: `${box.top}px` }}
+                  key={index}
+                >
+                  {box.word.split("").map((letter, index) => (
+                    <span
+                      key={index}
+                      className={letters[index] === letter ? "active" : ""}
+                    >
+                      {letter}
+                    </span>
+                  ))}
+                </div>
+              );
+            }
           })}
         </div>
         <div className="input-container">
